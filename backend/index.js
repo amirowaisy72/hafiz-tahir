87eb2232c57fe9874aa0027a43b28789f4586296
amirowaisy72 <amirowaisy72@gmail.com>
2024-10-18 15:21:41 +0500

    update

--- backend/index.js
+++ backend/index.js
@@ -7,12 +7,6 @@ var cors = require("cors");
 app.use(express.json());
 app.use(cors());
 
-// Test Root Route
-app.get('/', (req, res) => {
-  console.log("Root endpoint hit");
-  res.send("Hello from the API!");
-});
-
 //Available Routes
 app.use("/accounts", require("./routes/accounts.js"));
 app.use("/debitcredit", require("./routes/debitcredit.js"));
