a85db7015ad74081726f11cf2c5f4586e3dc62fc
amirowaisy72 <amirowaisy72@gmail.com>
2024-10-18 15:06:11 +0500

    tesst

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,13 @@ var cors = require("cors");
 
 app.use(express.json());
 app.use(cors());
+
+// Test Root Route
+app.get('/', (req, res) => {
+  console.log("Root endpoint hit");
+  res.send("Hello from the API!");
+});
+
 //Available Routes
 app.use("/accounts", require("./routes/accounts.js"));
 app.use("/debitcredit", require("./routes/debitcredit.js"));
@@ -22,32 +29,3 @@ app.listen(5000, () => {
   console.log("listening at port 5000");
 });
 
-// const mongoose = require('mongoose');
-// const express = require('express');
-// const app = express();
-// const cors = require('cors');
-// const connecToMongoose = require('./db');
-
-// app.use(express.json());
-// app.use(cors());
-
-// // Connect to MongoDB based on the provided database name
-// app.use((req, res, next) => {
-//   const mongoDbName = req.header('Mongo-Db-Name'); // Read the database name from the request header
-//   if (!mongoDbName) {
-//     return res.status(400).json({ message: 'Mongo-Db-Name header not provided.' });
-//   }
-
-//   // Modify the URL to include the dynamic database name
-//   const url = `mongodb+srv://amirowaisy72:iVVKYSj5rugATyVg@cluster0.mpb1bfz.mongodb.net/${mongoDbName}`;
-//   mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
-
-//   console.log('Connected to MongoDB:', mongoDbName);
-//   next();
-// });
-
-// // Define your routes here
-
-// app.listen(5000, () => {
-//   console.log('Listening at port 5000');
-// });
